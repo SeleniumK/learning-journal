1d19ef743831039863cf445757de954a6d62045a
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-03-24 12:45:56 -0700

    setuuuup

--- setup.py
+++ setup.py
@@ -55,5 +55,6 @@ setup(name='learning_journal',
       main = learning_journal:main
       [console_scripts]
       initialize_db = learning_journal.scripts.initializedb:main
+      load_api = learning_journal.apihit:main
       """,
       )
